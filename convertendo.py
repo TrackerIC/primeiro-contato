f4e4885969901678cf8574e1fad012ebbc9ec707
camandaroba06 <cristianogregorymc@gmail.com>
2023-09-04 17:56:11 -0300

    adicionada a funcionalidade de upload junto com diretório do arquivo salvo

--- convertendo.py
+++ convertendo.py
@@ -1,22 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 
 def convert(arquivo):
-  dataframe = pd.read_csv(arquivo)
+    dataframe = pd.read_csv(arquivo)
 
-  # Converter a coluna de Unix para data normal em horario de brasilia
-  dataframe['ncu.datetime'] = pd.to_datetime(dataframe['ncu.datetime'], unit='s')
-  dataframe['ncu.datetime'] = dataframe['ncu.datetime'].dt.tz_localize('UTC').dt.tz_convert('America/Sao_Paulo')
+    # Converter a coluna de Unix para data normal em horario de brasilia
+    dataframe["ncu.datetime"] = pd.to_datetime(dataframe["ncu.datetime"], unit="s")
+    dataframe["ncu.datetime"] = (
+        dataframe["ncu.datetime"]
+        .dt.tz_localize("UTC")
+        .dt.tz_convert("America/Sao_Paulo")
+    )
 
+    # Converter as colunas de radianos para graus
+    for i in range(0, 12):
+        dataframe[f"ncu.tcus[{i}].data.Position_a1_rad_s1"] = np.degrees(
+            dataframe[f"ncu.tcus[{i}].data.Position_a1_rad_s1"]
+        )
 
-  # Converter as colunas de radianos para graus
-  for i in range (0,12):
-    dataframe[f'ncu.tcus[{i}].data.Position_a1_rad_s1'] = np.degrees(dataframe[f'ncu.tcus[{i}].data.Position_a1_rad_s1'])
+    # Salvar o dataframe de volta como um novo arquivo CSV
+    novo_nome_arquivo = "seuarquivo_convertido.csv"
+    dataframe.to_csv(novo_nome_arquivo, index=False)
 
-  # Salvar o dataframe de volta como um novo arquivo CSV
-  novo_nome_arquivo = "seuarquivo_convertido.csv"
-  dataframe.to_csv(novo_nome_arquivo, index=False)
-
-  print("Conversão concluída e arquivo salvo:", novo_nome_arquivo)
+    print("Conversão concluída e arquivo salvo:", novo_nome_arquivo)
+    print(os.getcwd())
+    return os.path.join(os.getcwd(), novo_nome_arquivo)
--- mainApp.py
+++ mainApp.py
@@ -4,11 +4,15 @@ import json
 import matplotlib.pyplot as plt
 from tkinter import simpledialog, messagebox, filedialog
 import convertendo as cnv
+
 # Leitura do Arquivo
-arquivo = filedialog.askopenfilename(initialdir="/", title="Selecione a Planilha",
-                                    filetypes=[("Arquivos de Texto", "*.txt"),
-                                    ("Todos os Arquivos", "*.*")], 
-                                    defaultextension=".txt")
+arquivo = filedialog.askopenfilename(
+    initialdir="/",
+    title="Selecione a Planilha",
+    filetypes=[("Arquivos CSV", "*.csv"), ("Todos os Arquivos", "*.*")],
+    defaultextension=".txt",
+)
+print("local", arquivo)
 
 arquivo_tratado = cnv.convert(arquivo)
 
@@ -20,68 +24,69 @@ entrada_Dia = simpledialog.askinteger("Dia", "Digite o dia desejado:")
 entrada_TCU = simpledialog.askinteger("TCU", "Digite o TCU desejado:")
 
 
-
 # Convertendo a coluna 'ncu.datetime' para o formato de datetime
-planilha['ncu.datetime'] = pd.to_datetime(planilha['ncu.datetime'])
+planilha["ncu.datetime"] = pd.to_datetime(planilha["ncu.datetime"])
 
 # Filtrando os valores apenas do dia 16
-planilha_filtered = planilha[planilha['ncu.datetime'].dt.day == entrada_Dia]
+planilha_filtered = planilha[planilha["ncu.datetime"].dt.day == entrada_Dia]
 
 # Extraindo as horas dessa coluna filtrada, formatando para forma "hora:minuto"
-hours_of_day = planilha_filtered['ncu.datetime'].dt.strftime('%H:%M')
+hours_of_day = planilha_filtered["ncu.datetime"].dt.strftime("%H:%M")
 
 # Exibindo as horas como lista
 listaHoras_dia = hours_of_day.tolist()
-arquivo_selec = pd.read_csv('seuarquivo_convertido.csv', nrows=len(listaHoras_dia))
+arquivo_selec = pd.read_csv("seuarquivo_convertido.csv", nrows=len(listaHoras_dia))
 
 # Criando um JSON a partir da lista, apenas por estética do print
 print(json.dumps(listaHoras_dia, indent=4))
 
 
-padrao = 'ncu.tcus\[(\d+)\]\.data.Position_a1_rad_s1'
-colunas_com_padrao = [coluna for coluna in planilha.columns if pd.Series(coluna).str.match(padrao).any()]
+padrao = "ncu.tcus\[(\d+)\]\.data.Position_a1_rad_s1"
+colunas_com_padrao = [
+    coluna for coluna in planilha.columns if pd.Series(coluna).str.match(padrao).any()
+]
 # Calculando o número de colunas com o padrão
 numero_de_colunas_com_padrao = len(colunas_com_padrao)
 print(f"Número de colunas com o padrão '{padrao}': {numero_de_colunas_com_padrao}")
 
-diaMin = min(planilha['ncu.datetime'].dt.day)
-diaMax = max(planilha['ncu.datetime'].dt.day)
-
-
-
-
+diaMin = min(planilha["ncu.datetime"].dt.day)
+diaMax = max(planilha["ncu.datetime"].dt.day)
 
 
 # GRAFICO
-if (entrada_Dia >= diaMin and entrada_Dia <= diaMax) and (entrada_TCU>0 and entrada_TCU<=numero_de_colunas_com_padrao):
-
+if (entrada_Dia >= diaMin and entrada_Dia <= diaMax) and (
+    entrada_TCU > 0 and entrada_TCU <= numero_de_colunas_com_padrao
+):
     # Pega o valor do TCU
-    listaPosTCU = arquivo_selec[f'ncu.tcus[{entrada_TCU}].data.Position_a1_rad_s1'].tolist()
+    listaPosTCU = arquivo_selec[
+        f"ncu.tcus[{entrada_TCU}].data.Position_a1_rad_s1"
+    ].tolist()
     # Suas duas listas de valores
     lista_x = listaHoras_dia
     lista_y = listaPosTCU
 
     # Configura os eixos e formata o estilo do gráfico
-    plt.plot(lista_x, lista_y, 'r-')
+    plt.plot(lista_x, lista_y, "r-")
 
     # Rotacionar rótulos do eixo X para melhor legibilidade (opcional)
-    intervalo_legendas = 10 # Configura o intervalo dos termos do eixo
+    intervalo_legendas = 10  # Configura o intervalo dos termos do eixo
     plt.xticks(range(0, len(lista_x), intervalo_legendas), rotation=90)
 
-
     # Adicionar rótulos aos eixos
-    plt.xlabel('Data/Hora')
-    plt.ylabel(f'Posição TCU{entrada_TCU}')
+    plt.xlabel("Data/Hora")
+    plt.ylabel(f"Posição TCU{entrada_TCU}")
 
     # Adicionar título ao gráfico
-    plt.title(f'Gráfico de Posição do TCU{entrada_TCU} Dia {entrada_Dia}')
+    plt.title(f"Gráfico de Posição do TCU{entrada_TCU} Dia {entrada_Dia}")
 
     # Ajustar layout para evitar cortes nos rótulos
     plt.tight_layout()
 
     # Mostrar o gráfico
     plt.show()
-elif(entrada_Dia < diaMin or entrada_Dia > diaMax):
-    messagebox.showinfo("Alerta", f"O número do dia é menor que {diaMin} ou maior que {diaMax}")
-elif(entrada_TCU < 0 or entrada_TCU > numero_de_colunas_com_padrao):
+elif entrada_Dia < diaMin or entrada_Dia > diaMax:
+    messagebox.showinfo(
+        "Alerta", f"O número do dia é menor que {diaMin} ou maior que {diaMax}"
+    )
+elif entrada_TCU < 0 or entrada_TCU > numero_de_colunas_com_padrao:
     messagebox.showinfo("Alerta", f"O número do TCU não está presente na planilha")
